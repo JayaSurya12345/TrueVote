f309a12d6c484e707ca98ef75fb3e6cc7861b9f4
JayaSurya12345 <jayasurya.n2021csea@sece.ac.in>
2024-01-28 17:22:25 +0530

    TrueVote Allpication

---
+++ src/Data/Data.js
@@ -0,0 +1,32 @@
+const users = [
+    {
+      id: "7hZ2Q8cE7V",
+      name: 'Tomas Smith',
+      password: '1234',
+      type: 'user',
+      email: 'tomas19@aol.com'
+    },
+    {
+      id: "7hZ2Q8cE7X",
+      name: 'Elad Harel',
+      password: '1234',
+      type: 'admin',
+      email: 'test@gmail.com'
+    },
+    {
+      id: "8dN4Q2eB2W",
+      name: 'John Doe',
+      password: '7GTHPPL',
+      type: 'user',
+      email: 'johndoe@gmail.com'
+    },
+    {
+      id: "6mF9R3aD4X",
+      name: 'Jane Doe',
+      password: '6FDRKPL',
+      type: 'user',
+      email: 'janedoe@yahoo.com'
+    }
+  ];
+
+  export default users;
